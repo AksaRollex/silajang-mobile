93238f9b3ca8f2f26085d62ed0c1b2a861fe51fd
aldonyvr <aldony2006@gmail.com>
2024-10-22 08:30:21 +0700

    verifikasi verifikasi lhu

--- src/screens/pengujian/verifikasi/VerifikasiLhu/VerifikasiLhu.js
+++ src/screens/pengujian/verifikasi/VerifikasiLhu/VerifikasiLhu.js
@@ -15,6 +15,7 @@ import { APP_URL } from "@env";
 import Pdf from 'react-native-pdf';
 import RNFS, { downloadFile } from 'react-native-fs';
 import Toast from 'react-native-toast-message';
+import axios from '@/src/libs/axios';
 
 const currentYear = new Date().getFullYear();
 const generateYears = () => {
@@ -25,24 +26,22 @@ const generateYears = () => {
   return years;
 };
 
-const pengambilOptions = [
-  { id: 0, name: "Menunggu Konfirmasi" },
-  { id: 1, name: "Telah Diambil" },
+const VerifikasiOptions = [
+  { id: 7, name: "Menunggu Verifikasi" },
+  { id: 8, name: "Telah Diverifikasi/Disahkan" },
 ];
-
 const VerifikasiLhu = ({ navigation }) => {
   const [selectedYear, setSelectedYear] = useState(currentYear.toString());
   const filterOptions = generateYears();
-  const [selectedPengambil, setSelectedPengambil] = useState(0);
+  const [selectedVerifikasi, setSelectedVerifikasi] = useState(7);
   const paginateRef = useRef();
   const queryClient = useQueryClient();
-  const [selectedCetak, setSelectedCetak] = useState(1);
   const [modalVisible, setModalVisible] = useState(false);
   const [reportUrl, setReportUrl] = useState('');
 
   const { delete: showConfirmationModal, DeleteConfirmationModal } = useDelete({
     onSuccess: () => {
-      queryClient.invalidateQueries(['pengambil-sample']);
+      queryClient.invalidateQueries(['verifikasi/kepala-upt']);
       paginateRef.current?.refetch();
     },
     onError: (error) => {
@@ -50,9 +49,9 @@ const VerifikasiLhu = ({ navigation }) => {
     }
   });
 
-  const CetakSampling = async (item) => {
+  const PreviewVerifikasi = async (item) => {
     const authToken = await AsyncStorage.getItem('@auth-token');
-    setReportUrl(`${APP_URL}/api/v1/report/${item.uuid}/sampling?token=${authToken}`);
+    setReportUrl(`${APP_URL}/api/v1/report/${item.uuid}/preview-lhu?token=${authToken}`);
     setModalVisible(true);
   };
 
@@ -114,95 +113,167 @@ const VerifikasiLhu = ({ navigation }) => {
     }
   };
 
+  const Rollback = async (uuid) => {
+    try {
+      const authToken = await AsyncStorage.getItem('@auth-token');
+      const response = await axios.post(`/verifikasi/kepala-upt/${uuid}/rollback`, {
+        headers: {
+          'Authorization': `Bearer ${authToken}`,
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.status.success = 'success') {
+        queryClient.invalidateQueries(['verifikasi/kepala-upt']);
+        paginateRef.current?.refetch();
+        Toast.show({
+          type: 'success',
+          text1: 'Success',
+          text2: 'LHU has been rolled back successfully',
+        });
+      } else {
+        throw new Error('Failed to rollback');
+      }
+    } catch (error) {
+      console.error('Rollback error:', error.message);
+      Toast.show({
+        type: 'error',
+        text1: 'Error',
+        text2: `Failed to rollback: ${error.message}`,
+      });
+    }
+  };
+  const Verifikasi = async (uuid) => {
+    try {
+      const authToken = await AsyncStorage.getItem('@auth-token');
+      const response = await axios.post(`/verifikasi/kepala-upt/${uuid}/verify`, {
+        headers: {
+          'Authorization': `Bearer ${authToken}`,
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.data.status === 'success') { 
+        queryClient.invalidateQueries(['verifikasi/kepala-upt']);
+        paginateRef.current?.refetch();
+        Toast.show({
+          type: 'success',
+          text1: 'Success',
+          text2: 'LHU has been verified successfully',
+        });
+      } else {
+        throw new Error('Failed to Verify');
+      }
+    } catch (error) {
+      console.error('Verify error:', error.message);
+      Toast.show({
+        type: 'error',
+        text1: 'Error',
+        text2: `Failed to Verify: ${error.message}`,
+      });
+    }
+  };
+  
+  const TolakVerifikasi = async (uuid) => {
+    try {
+      const authToken = await AsyncStorage.getItem('@auth-token');
+      const response = await axios.post(`/verifikasi/kepala-upt/${uuid}/rollback-verif`, {
+        headers: {
+          'Authorization': `Bearer ${authToken}`,
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.status.success = 'success') {
+        queryClient.invalidateQueries(['verifikasi/kepala-upt']);
+        paginateRef.current?.refetch();
+        Toast.show({
+          type: 'success',
+          text1: 'Success',
+          text2: 'LHU has been reject back successfully',
+        });
+      } else {
+        throw new Error('Failed to reject');
+      }
+    } catch (error) {
+      console.error('Reject error:', error.message);
+      Toast.show({
+        type: 'error',
+        text1: 'Error',
+        text2: `Failed to reject: ${error.message}`,
+      });
+    }
+  };
 
-  const renderItem = ({ item }) => {
-    const isConfirmed = selectedPengambil === 1; // Telah Diambil
+  
 
-    const dropdownOptions = isConfirmed
-      ? [
-        { id: "Detail", title: "Detail", action: item => navigation.navigate("DetailPersetujuan", { uuid: item.uuid }) },
-        { id: "Cetak Sampling", title: "Cetak Sampling", action: item => CetakSampling({ uuid: item.uuid }) },
-        {
-          id: "Berita Acara",
-          title: "Berita Acara",
-          subactions: [
-            { id: "Berita Acara Pengambilan", title: "Berita Acara Pengambilan", action: item => BeritaAcara({ uuid: item.uuid }) },
-            { id: "Data Pengambilan", title: "Data Pengambilan", action: item => DataPengambilan({ uuid: item.uuid }) },
-          ]
-        }
-      ]
-      : [
-        { id: "Detail", title: "Detail", action: item => navigation.navigate("DetailPersetujuan", { uuid: item.uuid }) }
-      ];
+
+  const renderItem = ({ item }) => { 
+
+    const dropdownOptionsMenunggu = [
+      { id: "Preview", title: "Preview", action: (item) => PreviewVerifikasi(item) },
+      { id: "Verifikasi LHU", title: "Verifikasi LHU", action: (item) => Verifikasi(item.uuid) },
+      { id: "Tolak Verifikasi", title: "Tolak Verifikasi", action: (item) => TolakVerifikasi(item.uuid) },
+    ];
+    
+    const dropdownOptionsTelah = [
+      { id: "Preview", title: "Preview", action: (item) => PreviewVerifikasi(item) },
+    ];
+    if (item.status < 9 && item.status > 7) {
+      dropdownOptionsTelah.push({
+        id: "Rollback",
+        title: "Rollback",
+        action: (item) => Rollback(item.uuid),
+      });
+    }
+
+
+    const dropdownOptionsForItem = item.status > 7 ? dropdownOptionsTelah : dropdownOptionsMenunggu;
+    isConfirmed = item.text_status;
+  
 
     return (
-      <View
-        className="my-2 bg-[#f8f8f8] flex rounded-md border-t-[6px] border-indigo-900 p-5"
-        style={{
-          elevation: 4,
-        }}>
+      <View className="my-2 bg-[#f8f8f8] flex rounded-md border-t-[6px] border-indigo-900 p-5" style={{ elevation: 4 }}>
         <View className="flex-row justify-between items-center p-4 relative">
           <View className="flex-shrink mr-20">
             {isConfirmed ? (
               <>
-                <Text className="text-[18px] font-extrabold mb-1">
-                  {item.kode}
-                </Text>
-                <Text className="text-[15px] font-bold mb-2">
-                  {item.permohonan.user.nama}
-                </Text>
+                <Text className="text-[18px] font-extrabold mb-1">{item.kode}</Text>
+                <Text className="text-[15px] font-bold mb-2">{item.permohonan.user.nama}</Text>
               </>
             ) : (
-              <Text className="text-[18px] font-extrabold mb-3">
-                {item.permohonan.industri}
-              </Text>
+              <Text className="text-[15px] font-bold mb-2">{item.permohonan.user.nama}</Text>
             )}
             <Text className="text-[14px] mb-2">{item.lokasi}</Text>
-            <Text className="text-[14px] mb-2">Diambil pada: <Text className="font-bold ">{item.tanggal_pengambilan}</Text></Text>
-            <Text className="text-[14px] mb-2">Oleh: <Text className="font-bold">{item.pengambil?.nama}</Text></Text>
+            <Text className="text-[14px] font-bold mb-2">
+              PERATURAN : <Text className="font-normal">{item.peraturan?.nama}</Text>
+            </Text>
           </View>
           <View className="absolute right-1 flex-col items-center">
-            {!isConfirmed && (
-              <Text className={`text-[12px] font-bold px-2 py-1 rounded-md mb-3
-              ${item.kesimpulan_permohonan == 1 ? 'bg-green-100 text-green-500'
-                  : item.kesimpulan_permohonan == 2 ? 'bg-red-50 text-red-500'
-                    : 'bg-indigo-100 text-indigo-500'}`}>
-                {item.kesimpulan_permohonan == 1 ? 'Diterima'
-                  : item.kesimpulan_permohonan == 2 ? 'Ditolak'
-                    : 'Menunggu'}
+            <View className="bg-slate-100 rounded-md p-2 max-w-[150px] mb-2">
+              <Text className="text-[12px] text-indigo-600 font-bold text-right" numberOfLines={2} ellipsizeMode="tail">
+                {item.text_status}
               </Text>
-            )}
+            </View>
             <View className="my-2 ml-10">
-              <MenuView
-                title="dropdownOptions"
-                actions={dropdownOptions.map(option => ({
-                  ...option,
-                }))}
-                onPressAction={({ nativeEvent }) => {
-                  const selectedOption = dropdownOptions.find(
-                    option => option.title === nativeEvent.event,
-                  );
-                  const sub = dropdownOptions.find(
-                    option => option.subactions && option.subactions.some(
-                      suboption => suboption.title === nativeEvent.event
-                    )
-                  );
-                  if (selectedOption) {
-                    selectedOption.action(item);
-                  }
-                  if (sub) {
-                    const selectedSub = sub.subactions.find(sub => sub.title === nativeEvent.event);
-                    if (selectedSub) {
-                      selectedSub.action(item);
-                    }
-                  }
-                }}
-                shouldOpenOnLongPress={false}
-              >
-                <View>
-                  <Entypo name="dots-three-vertical" size={18} color="#312e81" />
-                </View>
-              </MenuView>
+            <MenuView
+              title="dropdownOptions"
+              actions={dropdownOptionsForItem}
+              onPressAction={({ nativeEvent }) => {
+                const selectedOption = dropdownOptionsForItem.find(
+                  option => option.title === nativeEvent.event
+                );
+
+                if (selectedOption) {
+                  selectedOption.action(item);
+                }
+              }}
+              shouldOpenOnLongPress={false}
+            >
+              <View>
+                <Entypo name="dots-three-vertical" size={18} color="#312e81" />
+              </View>
+            </MenuView>
             </View>
           </View>
         </View>
@@ -212,64 +283,59 @@ const VerifikasiLhu = ({ navigation }) => {
 
   return (
     <View className="bg-[#ececec] w-full h-full">
-      <View className=" p-4">
+      <View className="p-4">
         <View className="flex-row items-center space-x-2">
           <View className="flex-col w-full">
             <View className="flex-row items-center space-x-2 mb-4">
               <BackButton action={() => navigation.goBack()} size={26} />
               <View className="absolute left-0 right-2 items-center">
-                <Text className="text-[20px] font-bold ">Verifikasi LHU</Text>
-              </View> 
+                <Text className="text-[20px] font-bold">Verifikasi LHU</Text>
+              </View>
             </View>
 
             <View className="flex-row justify-center">
               <View className="mt-3 ml-[-10] mr-2"> 
                 <HorizontalScrollMenu
-                  items={pengambilOptions}
-                  selected={selectedPengambil}
-                  onPress={item => setSelectedPengambil(item.id)}
+                  items={VerifikasiOptions}
+                  selected={selectedVerifikasi}
+                  onPress={item => setSelectedVerifikasi(item.id)}
                   itemWidth={170}
                   scrollAreaStyle={{ height: 30, justifyContent: 'flex-start' }}
                   activeBackgroundColor={"#312e81"}
                   buttonStyle={{ marginRight: 10, borderRadius: 20, backgroundColor: "white" }}
                 />
-            </View>
-
-            <MenuView
-              title="filterOptions"
-              actions={filterOptions.map(option => ({
-                id: option.id.toString(),
-                title: option.title,
-              }))}
-              onPressAction={({ nativeEvent }) => {
-                const selectedOption = filterOptions.find(
-                  option => option.title === nativeEvent.event,
-                );
-                if (selectedOption) {
-                  setSelectedYear(selectedOption.title);
-                }
-              }}
-              shouldOpenOnLongPress={false}
-            >
-              <View style={{ marginEnd: 50 }}>
-                <MaterialCommunityIcons
-                  name="filter-menu-outline"
-                  size={24}
-                  color="white"
-                  style={{ backgroundColor: "#312e81", padding: 12, borderRadius: 8 }}
-                />
               </View>
-            </MenuView>
-          </View>
+
+              <MenuView
+                title="filterOptions"
+                actions={filterOptions.map(option => ({ id: option.id.toString(), title: option.title }))}
+                onPressAction={({ nativeEvent }) => {
+                  const selectedOption = filterOptions.find(option => option.title === nativeEvent.event);
+                  if (selectedOption) {
+                    setSelectedYear(selectedOption.title);
+                  }
+                }}
+                shouldOpenOnLongPress={false}
+              >
+                <View style={{ marginEnd: 50 }}>
+                  <MaterialCommunityIcons
+                    name="filter-menu-outline"
+                    size={24}
+                    color="white"
+                    style={{ backgroundColor: "#312e81", padding: 12, borderRadius: 8 }}
+                  />
+                </View>
+              </MenuView>
+            </View>
           </View>
         </View>
       </View>
 
       <Paginate
         ref={paginateRef}
-        url="/administrasi/pengambil-sample"
+        url="/verifikasi/kepala-upt"
         payload={{
-          status: selectedPengambil,
+          status: selectedVerifikasi,
           tahun: selectedYear,
           page: 1,
           per: 10,
@@ -291,7 +357,7 @@ const VerifikasiLhu = ({ navigation }) => {
               <TouchableOpacity onPress={() => {
                 handleDownloadPDF();
                 setModalVisible(false);
-              }} className=" p-2 rounded flex-row items-center">
+              }} className="p-2 rounded flex-row items-center">
                 <Feather name="download" size={21} color="black" />
               </TouchableOpacity>
             </View>
