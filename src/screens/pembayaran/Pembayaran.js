e5651ae23b57535e99d4fea5c6300603943324d1
Rio <rio@gmail.com>
2024-11-22 10:17:38 +0700

    push accordion pembayaran

--- src/screens/pembayaran/Pembayaran.js
+++ src/screens/pembayaran/Pembayaran.js
@@ -4,6 +4,7 @@ import {
   View,
   ScrollView,
   StyleSheet,
+  Text
 } from "react-native";
 import { Colors, Button } from "react-native-ui-lib";
 import { useNavigation } from "@react-navigation/native";
@@ -11,6 +12,7 @@ import { List } from "react-native-paper";
 import FontAwesome5Icon from "react-native-vector-icons/FontAwesome5";
 import FontAwesome6Icon from "react-native-vector-icons/FontAwesome6";
 import { TextFooter } from "../components/TextFooter";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Pembayaran = () => {
   const navigation = useNavigation();
@@ -30,34 +32,58 @@ const Pembayaran = () => {
   return (
     <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollViewContent}>
-        <List.Item
-          title="Pembayaran Dengan Pengujian"
-          left={props => (
-            <FontAwesome5Icon {...props} name="wallet" size={22} />
+
+      <View className=" p-4 flex flex-row items-center mt-2">
+        {/* <Ionicons name="document-text" size={22} style={{ color: "black" }}/> */}
+        <Text className="font-poppins-semibold ml-2 text-black text-lg">Pembayaran</Text>
+      </View>
+        <View>
+          <List.Item
+          style={{ borderTopLeftRadius: 10, borderTopRightRadius: 10 }}
+          title={<Text className="font-poppins-medium text-[15px]">Pembayaran</Text>}
+          left={() => (
+            <View className="bg-green-600 rounded-full ml-3">
+            <Ionicons name="wallet" size={17} color={'white'} style={{padding: 5}}/>
+          </View>
           )}
           right={props => <List.Icon {...props} icon="chevron-right" />}
-          className="bg-[#ffffff] border-black p-2"
+          className='bg-[#ffffff] border-black p-2 ml-3 mr-3'
           onPress={Pengujian}
-        />
-        <List.Item
-          title="Pembayaran Non Pengujian"
-          left={props => (
-            <FontAwesome5Icon {...props} name="credit-card" size={22} />
-          )}
-          right={props => <List.Icon {...props} icon="chevron-right" />}
-          className="bg-[#ffffff] border-black p-2"
-          onPress={NonPengujian}
-        />
-        <List.Item
-          title="Pembayaran Global"
-          left={props => (
-            <FontAwesome6Icon {...props} name="building-columns" size={22} />
-          )}
-          right={props => <List.Icon {...props} icon="chevron-right" />}
-          className="bg-[#ffffff] border-black p-2"
-          onPress={Global}
-        />
-        <TextFooter />
+          />
+          <View style={{ borderBottomWidth: 1, borderBottomColor: '#f0f0f0', marginHorizontal: 15 }} />
+        </View>
+
+        <View> 
+          <List.Item
+            title={<Text className="font-poppins-medium text-[15px]">Pembayaran Non Pengujian</Text>}
+            left={() => (
+              <View className="bg-green-600 rounded-full ml-3">
+              <Ionicons name="card" size={17} color={'white'} style={{padding: 5}}/>
+            </View>
+            )}
+            right={props => <List.Icon {...props} icon="chevron-right" />}
+            className='bg-[#ffffff] border-black p-2 ml-3 mr-3'
+            onPress={NonPengujian}
+            />
+            <View style={{ borderBottomWidth: 1, borderBottomColor: '#f0f0f0', marginHorizontal: 15 }} />
+          </View>
+
+          <View>
+            <List.Item
+            style={{ borderBottomLeftRadius: 10, borderBottomRightRadius: 10 }}
+            title={<Text className="font-poppins-medium text-[15px]">Pembayaran Global</Text>}
+            left={() => (
+              <View className="bg-green-600 rounded-full ml-3">
+              <Ionicons name="globe" size={17} color={'white'} style={{padding: 5}}/>
+            </View>
+            )}
+            right={props => <List.Icon {...props} icon="chevron-right" />}
+            className='bg-[#ffffff] border-black p-2 ml-3 mr-3'
+            onPress={Global}
+            />
+          <View style={{ borderBottomWidth: 1, borderBottomColor: '#f0f0f0', marginHorizontal: 15 }} />
+          </View> 
+      <TextFooter/>
       </ScrollView>
     </View>
   );
@@ -65,172 +91,15 @@ const Pembayaran = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // alignItems: "center",
     backgroundColor: "#ececec",
-    justifyContent: "flex-start", 
+    justifyContent: "flex-start", // Ensure content starts from the top
   },
-  headerContainer: {
-    width: "100%",
-    paddingVertical: 10,
-    backgroundColor: Colors.brand, 
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "row",
-    elevation: 4,
-  },
-  logo: {
-    width: 40,
-    height: 40,
-    marginTop: 8,
-  },
-  search: {
-    flexDirection: "row", 
-    alignItems: "center", 
-    justifyContent: "space-between", 
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    backgroundColor: "white",
-    borderRadius: 10,
-    marginVertical: 10,
-    width: "85%",
-  },
-  searchbar: {
-    flex: 1, 
-    backgroundColor: "white",
-    borderRadius: 10,
-    marginRight: 10, 
-  },
-  buttonSearch: {
-    backgroundColor: "#0D47A1",
-    padding: 10,
-    borderRadius: 10,
-  },
-  searchIcon: {
-    width: 24,
-    height: 24,
-  },
-  headerText: {
-    fontSize: 22,
-    color: "white",
-    fontWeight: "bold",
-    marginLeft: 10,
-    alignSelf: "center",
-  },
-
+ 
   scrollViewContent: {
-    flexGrow: 1, 
-    paddingBottom: 100, 
-  },
-  judul: {
-    fontSize: 17,
-    color: "black",
-    alignSelf: "center",
-    fontWeight: "bold",
-    marginBottom: 10,
-  },
-  row: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "center",
-  },
-
-  card: {
-    width: 360,
-    marginVertical: 10,
-    borderRadius: 15,
-    padding: 20,
-    backgroundColor: "#fff",
-    flexDirection: "row",
-    borderTopColor: Colors.brand,
-    borderTopWidth: 7,
-  },
-  cards: {
-    borderRadius: 10,
-    width: "70%",
-    marginBottom: 4,
-  },
-  cards2: {
-    borderRadius: 10,
-    width: "30%",
-    marginBottom: 4,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  cardTexts: {
-    fontSize: 15,
-    color: "black",
-  },
-
-  paginationContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center", 
-    marginTop: 20,
-    marginBottom: 40,
-  },
-  paginationButton: {
-    fontSize: 16,
-    color: "white",
-    fontWeight: "bold",
-    paddingVertical: 10,
-    width: 100,
-    backgroundColor: "#6b7fde",
-    borderRadius: 10,
-    marginHorizontal: 10, 
-    textAlign: "center", 
-  },
-  paginationText: {
-    fontSize: 16,
-    color: "black",
-    fontWeight: "bold",
-    marginHorizontal: 10,
-  },
-
-  ButtonDetail: {
-    paddingHorizontal: 14,
-    borderRadius: 5,
-    marginVertical: 5,
-    backgroundColor: "#4682B4",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "row", 
-  },
-  TextButton: {
-    color: "white",
-    fontSize: 12,
-    paddingVertical: 7,
-    fontWeight: "bold",
-  },
-  pending: {
-    color: "white",
-    backgroundColor: "green",
-    paddingVertical: 5,
-    borderRadius: 5,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  pendingText: {
-    color: "white",
-    fontSize: 15,
-    fontWeight: "bold",
-  },
-  icon: {
-    width: 20,
-    height: 20,
-    tintColor: "white",
-  },
-  disabledButton: {
-    color: "#ccc",
-  },
-  paginationNumber: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 5,
-    marginBottom: 80,
+    flexGrow: 1, // Ensures that ScrollView content is scrollable
+    paddingBottom: 100, // Add padding to avoid content being hidden behind the buttons
   },
 });
 
-export default Pembayaran;
+export default Pembayaran
